c3a1f583f5a83df9c1681d96a1c4a8581896fc53
Abdul Rehman <abduvidu3@gmail.com>
2026-05-13 23:41:19 +0500

    Created category

--- products/models.py
+++ products/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+    
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     sku = models.CharField(max_length=100, unique=True)
     inventory = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products_category')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
